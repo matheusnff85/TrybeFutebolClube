c14a5c7278bea71eb393be893a65f464a8bb4733
matheusnff85 <math.marinho154@gmail.com>
2022-09-29 16:59:35 -0300

    feat: adiciona o cors as bibliotecas e corrige o mock de user

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import errorHandler from './middlewares/errorHandler';
 import loginRouter from './routes/login.route';
 
@@ -23,6 +24,7 @@ class App {
     };
 
     this.app.use(express.json());
+    this.app.use(cors());
     this.app.use(accessControl);
     this.app.use(loginRouter);
     this.app.use(errorHandler);
--- app/backend/src/tests/mocks/userMockst.ts
+++ app/backend/src/tests/mocks/userMockst.ts
@@ -2,10 +2,10 @@ import { UserInterface } from '../../interfaces/userInterface';
 
 const oneUser: UserInterface  = {
   id: 1,
-  username: 'User',
+  username: 'miltaocrf',
   role: 'user',
-  email: 'userdas@user.com',
-  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
+  email: 'miltaocrf01@gmail.com',
+  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
 };
 
 export default oneUser;

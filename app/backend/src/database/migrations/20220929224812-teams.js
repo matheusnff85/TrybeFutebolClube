79d1aeea628f46b5100187c2d79a009acc704aa2
matheusnff85 <math.marinho154@gmail.com>
2022-09-29 19:50:53 -0300

    feat: adiciona migration e model da tabela teams

---
+++ app/backend/src/database/migrations/20220929224812-teams.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('teams');
+  }
+};
